5445cebcc93ec21e2f76af74820f5c69a0a96340
Cristiam Castillo <ccastillop@gmail.com>
2021-03-21 07:46:40 -0500

    better form and menu fix #11

--- app/javascript/controllers/showhide_controller.js
+++ app/javascript/controllers/showhide_controller.js
@@ -1,13 +1,27 @@
-import { Controller } from "stimulus";
-//import { useClickOutside } from 'stimulus-use'
+import { Controller } from "stimulus"
+import { useClickOutside } from 'stimulus-use'
 
 export default class extends Controller {
-  static targets = ["element", "innerElement", "svgElement", "innerSvgElement"];
+  static targets = ["showed", "hidden"]
+  static classes = [ "hidden" ]
   connect() {
+    useClickOutside(this)
   }
   toggle(event) {
-    this.elementTargets.forEach((el, i) => {
-      el.classList.toggle("hidden");
-    });
+    const elements = [...this.hiddenTargets, ...this.showedTargets]
+    elements.forEach((el, i) => {
+      el.classList.toggle(this.hiddenClass)
+    })
+  }
+  hide(event) {
+    this.showedTargets.forEach((el, i) => {
+      el.classList.remove(this.hiddenClass)
+    })
+    this.hiddenTargets.forEach((el, i) => {
+      el.classList.add(this.hiddenClass)
+    })
+  }
+  clickOutside(event) {
+    this.hide()
   }
 }
