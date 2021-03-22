ebfb0378956f8fa3b17c70b143bbd65a03582361
Cristiam Castillo <ccastillop@gmail.com>
2021-03-21 20:34:44 -0500

    seguridad y respuesta de candidato y partido fix #12

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -19,7 +19,7 @@ class AnswersController < ApplicationController
   def update
     respond_to do |format|
       if @answer.update(answer_params)
-        format.html { redirect_to answer_url, notice: "Tus respuestas han sido actualizadas" }
+        format.html { redirect_to [@answerer, @answer], notice: "Tus respuestas han sido actualizadas" }
         format.json { render :show, status: :ok, location: @answer }
         format.turbo_stream do
           @question = Question.find params[:question_id]
@@ -35,7 +35,7 @@ class AnswersController < ApplicationController
   def destroy
     @answer.destroy
     respond_to do |format|
-      format.html { redirect_to answer_url, notice: "He borrado tus respuestas" }
+      format.html { redirect_to [@answerer, @answer], notice: "He borrado tus respuestas" }
       format.json { head :no_content }
     end
   end
@@ -43,16 +43,19 @@ class AnswersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_answer
-      if user_signed_in?
-        Answer.where(user_id: nil, session_id: session[:session_id])
-          .update_all(user_id: current_user.id)
-      end
-      @answer = if user_signed_in? && policy_scope(Answer).any?
-        policy_scope(Answer).last
+      unless @answerer.present?
+        if user_signed_in?
+          Answer.where(user_id: nil, session_id: session[:session_id])
+            .update_all(user_id: current_user.id)
+        end
+        @answer = if user_signed_in? && policy_scope(Answer).any?
+          policy_scope(Answer).last
+        else
+          policy_scope(Answer).find_or_initialize_by(session_id: session[:session_id])
+        end
       else
-        policy_scope(Answer).find_or_initialize_by(session_id: session[:session_id])
+        @answer = @answerer.answer || @answerer.build_answer
       end
-      @answer.answerer ||= @answerer
     end
 
     # Only allow a list of trusted parameters through.
@@ -65,7 +68,7 @@ class AnswersController < ApplicationController
       @answerer = if params[:candidate_id]
         Candidate.find params[:candidate_id]
       elsif params[:party_id]
-        Party.find params[:candidate_id]
+        Party.find params[:party_id]
       else
         nil
       end
